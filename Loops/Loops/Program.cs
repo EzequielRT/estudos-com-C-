15e201c21c77dffa5fb48ef1e69ae1241b0bf57a
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-02-04 21:27:49 -0300

    Adicionar arquivos de projeto.

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //------------------------WHILE------------------------
+
+         /* int a = 1;
+            while(a <= 10) 
+            {
+                if (a == 5) 
+                {
+                    a++;
+                    continue
+                  //break; -> FINALIZA A OPERAÇÃO
+                }
+                Console.WriteLine(a);
+                a++;
+            }
+
+            Console.ReadLine(); */
+
+          //------------------------DO-WHILE------------------------
+
+         /* int a = 1;
+            do
+            {
+                if (a == 3) 
+                {
+                   break;
+                }
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a <= 10); 
+         
+            Console.ReadLine();*/
+
+          //------------------------FOR------------------------
+
+         /* for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine(); */
+
+          //------------------------FOREACH------------------------
+
+         /* int[] a = { 1, 2, 3, 4, 5 };
+
+            foreach (int valor in a)
+            {
+                Console.WriteLine(valor);
+            }
+
+            //Mesmo exemplo usando FOR:
+            Console.WriteLine("-----------------");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            Console.ReadLine(); */
+
+            Looping(1);
+            Console.ReadLine();
+        }
+
+        //------------------------FUNCAO-RECURSIVA------------------------
+
+        public static void Looping(int a)
+        {
+            Console.WriteLine(a);
+            if (a < 5) 
+            {   
+                Looping(a + 1);
+            }
+        }
+    }
+}
---
+++ Operadores_Logicos_E_Condicionais/Operadores_Logicos_E_Condicionais/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Logicos_E_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------ELSE-IF------------------------
+            var a = 2;
+            if (a == 1)
+            {
+                Console.WriteLine("Entrou na condição do if");
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine("Caiu no bloco do senão se a = 2");
+            }
+            else
+            {
+                Console.WriteLine("Caiu no bloco do else");
+            }
+
+            //------------------------SWITCH-CASE------------------------
+
+            var b = 3;
+            switch (b)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            //------------------------OPERADOR-TERNARIO------------------------
+
+            var x = 2;
+            string teste = (x == 1 ? "X é igual a 1" : "X é diferente de 1");
+
+            Console.WriteLine(teste);
+
+            Console.ReadLine();
+        }
+    }
+}
