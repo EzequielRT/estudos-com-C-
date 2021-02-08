44ff2fb8f4370247c0e1f674f7103f65cd7e1245
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-02-08 00:48:17 -0300

    Estudo sobre Funcoes Recursivas

---
+++ Funcoes_Recursivas/Funcoes_Recursivas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes_Recursivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+            Console.ReadLine();
+        }
+
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\Arquivos\arq" + numeroArquivo + ".txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+
+            string arquivoComCaminho2 = @"C:\Arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);       // Recursividade
+            }
+        }
+    }
+}
