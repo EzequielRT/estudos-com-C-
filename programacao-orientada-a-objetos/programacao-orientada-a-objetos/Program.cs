333c21927415456c3045832e7f7618f892fd3009
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-02-14 21:53:58 -0300

    Projeto para desenvolvimento orientado a objetos

---
+++ programacao-orientada-a-objetos/programacao-orientada-a-objetos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tela;
+
+namespace programacao_orientada_a_objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Criar();
+        }
+    }
+}
