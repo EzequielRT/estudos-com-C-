4e6f4f3c4292b7281b7fec28b811a75d75c10860
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-02-23 22:15:06 -0300

    POO - Classes e Instancias

--- programacao-orientada-a-objetos/programacao-orientada-a-objetos/Diretorio/Arquivo.cs
+++ programacao-orientada-a-objetos/programacao-orientada-a-objetos/Diretorio/Arquivo.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Diretorio
 {
     class Arquivo
     {
+        private static string caminhoArquivo()
+        {
+            return ConfigurationManager.AppSettings["CaminhoArquivos"];
+        }
         public static void Ler(int numeroArquivo)
         {
-            string arquivoComCaminho = @"C:\Arquivos\arq" + numeroArquivo + ".txt";
+            string arquivoComCaminho = caminhoArquivo() + "arq" + numeroArquivo + ".txt";
             Console.WriteLine("============ Lendo arquivo ============ \n" + arquivoComCaminho + "\n=====================");
             if (File.Exists(arquivoComCaminho))
             {
@@ -25,7 +30,7 @@ namespace Diretorio
                 }
             }
 
-            string arquivoComCaminho2 = @"C:\Arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            string arquivoComCaminho2 = caminhoArquivo() + "arq" + (numeroArquivo + 1) + ".txt";
             if (File.Exists(arquivoComCaminho2))
             {
                 Arquivo.Ler(numeroArquivo + 1);
--- programacao-orientada-a-objetos/programacao-orientada-a-objetos/Program.cs
+++ programacao-orientada-a-objetos/programacao-orientada-a-objetos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,22 @@ namespace programacao_orientada_a_objetos
     {
         static void Main(string[] args)
         {
-            Menu.Criar();
+            //Menu.Criar();
+
+            var clientes = Cliente.LerClientes();  // dados estáticos
+
+            var cliente = new Cliente();  // dados instanciados
+            cliente.Nome = "danilo";
+            cliente.Telefone = "515558184949";
+            cliente.Telefone = "1515191859";
+            cliente.Gravar();
+
+            /*var cliente2 = new Cliente();  // dados instanciados
+            cliente2.Nome = "sheila";
+            cliente2.Telefone = "51564615191";
+            cliente2.Telefone = "6515615919";
+            cliente2.Gravar();
+            */
         }
     }
 }
