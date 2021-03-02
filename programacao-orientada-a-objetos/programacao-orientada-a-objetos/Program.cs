85e2b926e8e171fb340a3122dbff77892ba5f824
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-01 23:03:33 -0300

    POO - Heranca

--- programacao-orientada-a-objetos/programacao-orientada-a-objetos/Program.cs
+++ programacao-orientada-a-objetos/programacao-orientada-a-objetos/Program.cs
@@ -12,7 +12,38 @@ namespace programacao_orientada_a_objetos
     {
         static void Main(string[] args)
         {
-            Menu.Criar();
+            //Menu.Criar();
+
+            Console.WriteLine("================ Cadastro de Cliente ===================");
+            Cliente c = new Cliente();
+            c.Nome = "sss";
+            c.Telefone = "23333";
+            c.CPF = "151615";
+            c.Gravar();
+
+            foreach (Cliente cl in Cliente.LerClientes())
+            {
+                Console.WriteLine(cl.Nome);
+                Console.WriteLine(cl.Telefone);
+                Console.WriteLine(cl.CPF);
+                Console.WriteLine("===================================");
+            }
+
+            Console.WriteLine("================ Cadastro de Usuario ===================");
+            Usuario u = new Usuario();
+            u.Nome = "sss";
+            u.Telefone = "23333";
+            u.CPF = "151615";
+            u.Gravar();
+
+            foreach (Usuario us in Usuario.LerUsuarios())
+            {
+                Console.WriteLine(us.Nome);
+                Console.WriteLine(us.Telefone);
+                Console.WriteLine(us.CPF);
+                Console.WriteLine("===================================");
+            }
+            Console.ReadLine();
         }
     }
 }
