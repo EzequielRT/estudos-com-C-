db75daa9e873fcbfff952046f947a9407abc7d62
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-25 23:32:01 -0300

    Windows Forms - TabControl

--- windows-forms-application/windows-forms-application/FrmTabControl.cs
+++ windows-forms-application/windows-forms-application/FrmTabControl.cs
@@ -14,5 +14,25 @@ namespace windows_forms_application
         {
             InitializeComponent();
         }
+
+        private void FrmTabControl_Load(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab("tabPage3");
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab("tabPage1");
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab("tabPage2");
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab("tabPage3");
+        }
     }
 }
